edfcb0b2846d3f6b23ee31a377dc32e797992db2
ByeongHunKim <rkwhr912@naver.com>
2023-04-18 12:39:19 +0900

    constant values

---
+++ constant_example.ts
@@ -0,0 +1,15 @@
+export const TEST1_TEST2_LP_V4_POOL_KEY =
+
+export const RAYDIUM_LIQUIDITY_JSON = 'https://api.raydium.io/v2/sdk/liquidity/mainnet.json';
+
+export const SOLANA_RPC = 'https://api.mainnet-beta.solana.com';
+
+export const WALLET_ADDRESS =
+
+export const TEST_TOKEN1_MINT =
+
+export const TOKEN1_ACCOUNT =
+
+export const TEST_TOKEN2_MINT =
+
+export const PRIVATE_KEY =
